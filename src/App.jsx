1848d7908ada114d1c3a38def709c326c18e543b
Olivier <oliviergg@uma.es>
2023-03-01 18:39:44 +0100

    Boton de comprar y componente Funko

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Layout} from './pages/Layout';
 import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import {Login} from "./pages/Login";
 import {SignUp} from "./pages/SignUp";
+import {Funko} from "./components/Funko";
 import './assets/styles/App.css'
 
 export const BASE_PATH = "/Proyecto-Interfaces-Grupo-H";
@@ -15,6 +16,7 @@ function App() {
                     <Route index element={<Home/>}/>
                     <Route path={"Login"} element={<Login/>}/>
                     <Route path={"SignUp"} element={<SignUp/>}/>
+                    <Route path={"Funko"} element={<Funko name="prueba" price="7.00" image="https://m.media-amazon.com/images/I/61rvig8NbQL.__AC_SX300_SY300_QL70_ML2_.jpg"/>}/>
                     <Route path="*" element={<h1>404: Not Found</h1>}/>
                     {/*<Route path="*">*/}
                     {/*    <Redirect to="/Proyecto-Interfaces-Grupo-H"/>*/}
