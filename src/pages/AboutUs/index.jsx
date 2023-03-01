bb05cea113108cce79f0d34231b864798ec3d996
4lvaro_22 <pl.alvaro43@uma.es>
2023-03-01 19:46:30 +0100

    Changing About Us and Terms and Conditions pages

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,9 +1,18 @@
 import grupoPersonas from "../../assets/images/grupoPersonas.jpg";
+import {LoremIpsum} from "../../components/LoremIpsum";
 
 export const AboutUs = () => {
     return (
-        <div className="App">
-            <img src={grupoPersonas} className="float-right" alt="fotoGrupo" />
+        <div className="my-5">
+            <h1 className="text-center">
+                <strong>FunkoShop</strong>
+                <small class="text-muted mx-3">Grupo-H</small>
+            </h1>
+            <img src={grupoPersonas} className="float-start me-auto" alt="fotoGrupo" />
+            <LoremIpsum />
         </div>
     );
 }
+
+
+export default AboutUs;
