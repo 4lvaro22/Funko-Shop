56c5e096996007e560f45fa71aeba0422d68e291
josefco-02 <josefco_02@uma.es>
2023-03-01 18:28:40 +0100

    Cambios en header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import {Login} from "./pages/Login";
 import {AboutUs} from "./pages/AboutUs";
 import { TermsAndConditions } from './pages/TermsAndConditions';
 import {SignUp} from "./pages/SignUp";
-import './assets/styles/App.css'
+import { Cart } from './pages/Cart';
+import './assets/styles/App.css';
 
 export const BASE_PATH = "/Proyecto-Interfaces-Grupo-H";
 
@@ -19,6 +20,7 @@ function App() {
                     <Route path={"AboutUs"} element={<AboutUs/>}/>
                     <Route path={"TermsAndConditions"} element={<TermsAndConditions/>}/>
                     <Route path={"SignUp"} element={<SignUp/>}/>
+                    <Route path={"Cart"} element={<Cart/>}/>
                     <Route path="*" element={<h1>404: Not Found</h1>}/>
                     {/*<Route path="*">*/}
                     {/*    <Redirect to="/Proyecto-Interfaces-Grupo-H"/>*/}
