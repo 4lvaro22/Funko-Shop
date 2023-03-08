3b47a29c6f257179ceeb05a88e0f75eeaa28f34c
4lvaro_22 <pl.alvaro43@uma.es>
2023-03-08 09:51:53 +0100

    8 marzo 2023

---
+++ src/components/Informacion.jsx
@@ -0,0 +1,9 @@
+
+export const Informacion = () => {
+    return (
+        <i class="bi bi-info-circle"></i>
+    );
+}
+
+
+export default LoremIpsum;
--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import cart from "../../assets/images/cart.svg";
+import icono from "../../assets/images/funkopop.png";
 
 const divStyle = {
     color: 'white',
@@ -8,10 +9,20 @@ const divStyle = {
 export const Header = () => {
     return (
         <>
-            <header><h1><Link to="/" style={divStyle}>FunkoShop</Link></h1>
-                <Link to={"/Login"} style={divStyle}><button id="login">Log in</button></Link>
-                <Link to={"/SignUp"} style={divStyle}><button id="signup">Sign up</button></Link>
-                <Link to={"/Cart"} style={divStyle}><button id="cart"><img src={cart}></img>Cart</button></Link>
+            <header>
+                <div className="container mw-100 center-block">
+                    <span className="col-4">
+                        <Link to={"/"}><img className="w-50" src={icono}></img></Link>
+                    </span>
+                    <span className="col-4 text-center">
+                        <h1>FunkoShop</h1>
+                    </span>
+                    <span className="col-4">
+                        <Link to={"/Login"} style={divStyle}><button id="login">Log in</button></Link>
+                        <Link to={"/SignUp"} style={divStyle}><button id="signup">Sign up</button></Link>
+                        <Link to={"/Cart"} style={divStyle}><button id="cart"><img src={cart}></img>Cart</button></Link>
+                    </span>
+                </div>
             </header>
         </>
     )
