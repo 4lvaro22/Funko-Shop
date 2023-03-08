fd3ff9e155814a6e90d83fc789308d634218b261
josefco-02 <josefco_02@uma.es>
2023-03-08 09:44:52 +0100

    deleted common styles

--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -1,17 +1,14 @@
 import {Link} from "react-router-dom";
 import cart from "../../assets/images/cart.svg";
 
-const divStyle = {
-    color: 'white',
-    textDecoration: 'none',
-};
+
 export const Header = () => {
     return (
         <>
-            <header><h1><Link to="/" style={divStyle}>FunkoShop</Link></h1>
-                <Link to={"/Login"} style={divStyle}><button id="login">Iniciar sesión</button></Link>
-                <Link to={"/SignUp"} style={divStyle}><button id="signup">Registrarme</button></Link>
-                <Link to={"/Cart"} style={divStyle}><button id="cart"><img src={cart}></img>Carrito</button></Link>
+            <header><h1><Link to="/">FunkoShop</Link></h1>
+                <Link to={"/Login"}><button id="login">Iniciar sesión</button></Link>
+                <Link to={"/SignUp"}><button id="signup">Registrarme</button></Link>
+                <Link to={"/Cart"}><button id="cart"><img src={cart}></img>Carrito</button></Link>
             </header>
         </>
     )
