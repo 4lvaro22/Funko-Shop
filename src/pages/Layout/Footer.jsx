ce4226cd3ba30ceff327069fbbd4387ba9a1a8b6
4lvaro_22 <pl.alvaro43@uma.es>
2023-03-09 17:56:13 +0100

    Footer realizado

--- src/pages/Layout/Footer.jsx
+++ src/pages/Layout/Footer.jsx
@@ -1,35 +1,49 @@
-import instagram from "../../assets/images/Instagram-Icon.png";
-import facebook from "../../assets/images/Facebook.png";
-import twitter from "../../assets/images/twitter.png";
 import spanish from "../../assets/images/Spain.png";
 import {Link} from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export const Footer = () => {
     return (
         <>
             <footer>
-                <ul id="redes">
-                    <li id="instagram"> <img src={instagram} alt="Instagram"/><a href="https://www.instagram.com/">FunkoShopInterfaces</a></li>
-                    <li id="facebook"><img src={facebook} alt="Facebook"/><a href="https://es-es.facebook.com/">FunkoShopInterfaces</a></li>
-                    <li id="twitter"><img src={twitter} alt='Twitter'/><a href="https://twitter.com/">FunkoShopInterfaces</a></li>
-                </ul>
-                <div id="info">
-                    <div>
-                        <Link to={"/TermsAndConditions"}>Términos y Condiciones</Link>
-                        <Link to={"/AboutUs"}>Sobre nosotros</Link>
-                        <a href="#">Contacto</a>
-                        <a href="#">Localización</a>
-                    </div>
+                <div id="footer" className="container-fluid bg-dark">
+                    <div className="row m-2 p-3">
+                        <div className="col-2" id="redes">
+                            <div className="row" id="instagram"> 
+                                <div>
+                                    <a className="link-light text-decoration-none" href="https://www.instagram.com/"><i class="bi bi-instagram"></i> <strong>FunkoShopInterfaces</strong></a>
+                                </div>
+                            </div>
+                            <div className="row" id="facebook"> 
+                                <div>
+                                    <a className="link-light text-decoration-none" href="https://es-es.facebook.com/"><i class="bi bi-facebook"></i> <strong>FunkoShopInterfaces</strong></a>
+                                </div>
+                            </div>
+                            <div className="row" id="twitter"> 
+                                <div>
+                                        <a className="link-light text-decoration-none" href="https://twitter.com/"><i class="bi bi-twitter"></i> <strong>FunkoShopInterfaces</strong></a>
+                                </div>
+                            </div>
+                        </div>
 
-                    <div>
-                        <p>© 2023 FunkoShopInterfaces</p>
+                        <div className="col-8" id="info">
+                            <span className="block d-flex justify-content-center m-2">
+                                    <Link to={"/TermsAndConditions"} className="text-white text-decoration-none mx-2">Términos y Condiciones </Link>
+                                    <Link to={"/AboutUs"} className="text-white text-decoration-none mx-2">Sobre nosotros</Link>
+                                    <a href="#" className="link-light text-decoration-none mx-2">Contacto</a>
+                                    <a href="#" className="link-light text-decoration-none mx-2">Localización</a>
+                            </span>
+
+                            <span className="block d-flex justify-content-center m-2">
+                                <p className="text-white justify-content-center">© 2023 FunkoShopInterfaces</p>
+                            </span>
+                        </div>
+
+                        <div className="col-2 d-flex justify-content-end" id="idioma">
+                            <p className="text-white m-4"><img src={spanish} alt="Idioma Español"/> ES</p>
+                        </div>
                     </div>
                 </div>
-
-                <div id="idioma">
-                    <p><img src={spanish} alt="Idioma Español"/>ES</p>
-                </div>
-
             </footer>
         </>
     )
