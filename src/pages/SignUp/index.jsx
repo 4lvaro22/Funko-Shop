bf0de6d72968a8916a376f8fa19e805d0abc8508
Olivier-GG <100834647+Olivier-GG@users.noreply.github.com>
2023-03-15 10:59:47 +0100

    arreglo problemas del merge

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,45 +1,46 @@
-import logo from "../../assets/images/logo.svg";
-import { Link } from "react-router-dom";
-import logoFunko from "../../assets/images/funkoIcono.png";
+import logoFunko from '../../assets/images/funkoIcono.png';
 
 export const SignUp = () => {
   return (
-    <div class="container mt-5">
-      <div class="row justify-content-md-center my-">
-        <div className="col col-lg-3">
-          <img src={logoFunko}></img>
+    <div class='container mt-5'>
+      <div class='row justify-content-md-center my-'>
+        <div className='col col-lg-3'>
+          <img src={logoFunko} />
         </div>
-        <div class="col col-lg-3">
-          <div className="Auth-form-container">
-            <form className="Auth-form">
-              <div className="Auth-form-content">
-                <h2 className="Auth-form-title">Registrarme</h2>
-                <div className="form-group mt-3">
+        <div class='col col-lg-3'>
+          <div className='Auth-form-container'>
+            <form className='Auth-form'>
+              <div className='Auth-form-content'>
+                <h2 className='Auth-form-title'>Registrarme</h2>
+                <div className='form-group mt-3'>
                   <label>Nombre</label>
-                  <input type="name" className="form-control mt-1" />
+                  <input type='name' className='form-control mt-1' />
                 </div>
-                <div className="form-group mt-3">
+                <div className='form-group mt-3'>
                   <label>Apellido</label>
-                  <input type="surname" className="form-control mt-1" />
+                  <input type='surname' className='form-control mt-1' />
                 </div>
-                <div className="form-group mt-3">
+                <div className='form-group mt-3'>
                   <label>Correo Electrónico</label>
-                  <input type="email" className="form-control mt-1" />
+                  <input type='email' className='form-control mt-1' />
                 </div>
-                <div className="form-group mt-3">
+                <div className='form-group mt-3'>
                   <label>Contraseña</label>
-                  <input type="password" className="form-control mt-1" />
+                  <input type='password' className='form-control mt-1' />
                 </div>
-                <div className="d-grid gap-2 mt-5">
-                  <button type="submit" className="btn btn-primary">
+                <div className='d-grid gap-2 mt-5'>
+                  <button type='submit' className='btn btn-primary'>
                     Enviar
                   </button>
                 </div>
               </div>
             </form>
           </div>
-        </div>   
-    );
-}
+        </div>
+      </div>
+    </div>
+
+  );
+};
 
 export default SignUp;
