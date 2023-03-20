82e533f38e13ed40be7a90239fb7bed8b735b051
4lvaro_22 <pl.alvaro43@uma.es>
2023-03-20 19:26:28 +0100

    first version of FAQ page

--- src/pages/Layout/Footer.jsx
+++ src/pages/Layout/Footer.jsx
@@ -1,90 +1,90 @@
-import spanish from "../../assets/images/Spain.png";
-import { Link } from "react-router-dom";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import spanish from '../../assets/images/Spain.png';
+import { Link } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export const Footer = () => {
   return (
     <>
       <footer>
         <div
-          id="footer"
-          className="container-fluid bg-dark position-absolute top-100 start-50 translate-middle"
+          id='footer'
+          className='container-fluid bg-dark position-relative'
         >
-          <div className="row m-2 p-3">
-            <div className="col-2" id="redes">
-              <div className="row" id="instagram">
+          <div className='row p-3'>
+            <div className='col-2' id='redes'>
+              <div className='row' id='instagram'>
                 <div>
                   <a
-                    className="link-light text-decoration-none"
-                    href="https://www.instagram.com/"
+                    className='link-light text-decoration-none'
+                    href='https://www.instagram.com/'
                   >
-                    <i class="bi bi-instagram"></i>{" "}
+                    <i class='bi bi-instagram' />{' '}
                     <strong>FunkoShopInterfaces</strong>
                   </a>
                 </div>
               </div>
-              <div className="row" id="facebook">
+              <div className='row' id='facebook'>
                 <div>
                   <a
-                    className="link-light text-decoration-none"
-                    href="https://es-es.facebook.com/"
+                    className='link-light text-decoration-none'
+                    href='https://es-es.facebook.com/'
                   >
-                    <i class="bi bi-facebook"></i>{" "}
+                    <i class='bi bi-facebook' />{' '}
                     <strong>FunkoShopInterfaces</strong>
                   </a>
                 </div>
               </div>
-              <div className="row" id="twitter">
+              <div className='row' id='twitter'>
                 <div>
                   <a
-                    className="link-light text-decoration-none"
-                    href="https://twitter.com/"
+                    className='link-light text-decoration-none'
+                    href='https://twitter.com/'
                   >
-                    <i class="bi bi-twitter"></i>{" "}
+                    <i class='bi bi-twitter' />{' '}
                     <strong>FunkoShopInterfaces</strong>
                   </a>
                 </div>
               </div>
             </div>
 
-            <div className="col-8" id="info">
-              <span className="block d-flex justify-content-center m-2">
+            <div className='col-8' id='info'>
+              <span className='block d-flex justify-content-center m-2'>
                 <Link
-                  to={"/TermsAndConditions"}
-                  className="text-white text-decoration-none mx-2"
+                  to='/TermsAndConditions'
+                  className='text-white text-decoration-none mx-2'
                 >
-                  Términos y Condiciones{" "}
+                  Términos y Condiciones{' '}
                 </Link>
                 <Link
-                  to={"/AboutUs"}
-                  className="text-white text-decoration-none mx-2"
+                  to='/AboutUs'
+                  className='text-white text-decoration-none mx-2'
                 >
                   Sobre nosotros
                 </Link>
-                <a href="#" className="link-light text-decoration-none mx-2">
+                <a href='#' className='link-light text-decoration-none mx-2'>
                   Contacto
                 </a>
-                <a href="https://goo.gl/maps/douTAYgW9ja5h5VN6" className="link-light text-decoration-none mx-2">
+                <a href='https://goo.gl/maps/douTAYgW9ja5h5VN6' className='link-light text-decoration-none mx-2'>
                   Localización
                 </a>
-                <Link 
-                  to={"/Faq"}
-                  className="text-white text-decoration-none mx-2"
+                <Link
+                  to='/Faq'
+                  className='text-white text-decoration-none mx-2'
                 >
                   ¿Necesitas Ayuda?
                 </Link>
               </span>
 
-              <span className="block d-flex justify-content-center m-2">
-                <p className="text-white justify-content-center">
+              <span className='block d-flex justify-content-center m-2'>
+                <p className='text-white justify-content-center'>
                   © 2023 FunkoShopInterfaces
                 </p>
               </span>
             </div>
 
-            <div className="col-2 d-flex justify-content-end" id="idioma">
-              <p className="text-white m-4">
-                <img src={spanish} alt="Idioma Español" /> ES
+            <div className='col-2 d-flex justify-content-end' id='idioma'>
+              <p className='text-white m-4'>
+                <img src={spanish} alt='Idioma Español' /> ES
               </p>
             </div>
           </div>
--- src/pages/Profile/form.jsx
+++ src/pages/Profile/form.jsx
@@ -3,87 +3,87 @@ const years = [2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026];
 
 export const PaymentForm = () => {
   return (
-    <div className="padding m-4">
-      <div className="row">
-        <div className="col-sm-9">
-          <div className="card">
-            <form className="needs-validation">
-              <div className="card-header ">
+    <div className='padding m-4'>
+      <div className='row'>
+        <div className='col-sm-9'>
+          <div className='card'>
+            <form className='needs-validation'>
+              <div className='card-header '>
                 <strong>Nueva tarjeta de crédito</strong>
                 {/* <small>enter your card details</small> */}
               </div>
-              <div className="card-body g-3 row">
-                <div className="col-2"> </div>
-                <div className="col-8">
-                  <div className="row">
-                    <div className="col-5">
-                      <div className="form-group">
-                        <label for="name" className="form-label">
+              <div className='card-body g-3 row'>
+                <div className='col-2'> </div>
+                <div className='col-8'>
+                  <div className='row'>
+                    <div className='col-5'>
+                      <div className='form-group'>
+                        <label for='name' className='form-label'>
                           Nombre del Dueño
                         </label>
                         <input
-                          className="form-control"
-                          id="name"
-                          type="text"
-                          placeholder="Enter your name"
+                          className='form-control'
+                          id='name'
+                          type='text'
+                          placeholder='Enter your name'
                           required
                         />
                       </div>
                     </div>
                   </div>
 
-                  <div className="row">
-                    <div className="col-5">
-                      <div className="form-group">
-                        <label for="cardNumber">Número de Tarjeta</label>
+                  <div className='row'>
+                    <div className='col-5'>
+                      <div className='form-group'>
+                        <label for='cardNumber'>Número de Tarjeta</label>
 
-                        <div className="input-group">
+                        <div className='input-group'>
                           <input
-                            id="cardNumber"
-                            className="form-control"
-                            type="text"
-                            placeholder="0000 0000 0000 0000"
+                            id='cardNumber'
+                            className='form-control'
+                            type='text'
+                            placeholder='0000 0000 0000 0000'
                             maxLength={16}
                             minLength={16}
-                            autocomplete="email"
+                            autocomplete='email'
                             required
                           />
-                          <div className="input-group-append">
-                            <span className="input-group-text">
-                              <i className="bi bi-credit-card"></i>
+                          <div className='input-group-append'>
+                            <span className='input-group-text'>
+                              <i className='bi bi-credit-card' />
                             </span>
                           </div>
                         </div>
                       </div>
                     </div>
-                <div className="col-2"> </div>
+                    <div className='col-2'> </div>
                   </div>
 
-                  <div className="row">
-                    <div className="form-group col-2 ">
-                      <label for="ccmonth">Mes</label>
-                      <select className="form-control" id="ccmonth">
+                  <div className='row'>
+                    <div className='form-group col-2 '>
+                      <label for='ccmonth'>Mes</label>
+                      <select className='form-control' id='ccmonth'>
                         {months.map((month) => (
                           <option value={month}>{month}</option>
                         ))}
                       </select>
                     </div>
-                    <div className="form-group col-3">
-                      <label for="ccyear">Año</label>
-                      <select className="form-control" id="ccyear">
+                    <div className='form-group col-3'>
+                      <label for='ccyear'>Año</label>
+                      <select className='form-control' id='ccyear'>
                         {years.map((year) => (
                           <option value={year}>{year}</option>
                         ))}
                       </select>
                     </div>
-                    <div className="col-3">
-                      <div className="form-group">
-                        <label for="cvv">CVV/CVC</label>
+                    <div className='col-3'>
+                      <div className='form-group'>
+                        <label for='cvv'>CVV/CVC</label>
                         <input
-                          className="form-control"
-                          id="cvv"
-                          type="text"
-                          placeholder="123"
+                          className='form-control'
+                          id='cvv'
+                          type='text'
+                          placeholder='123'
                           maxLength={3}
                           minLength={3}
                           required
@@ -93,12 +93,12 @@ export const PaymentForm = () => {
                   </div>
                 </div>
               </div>
-              <div className="card-footer">
+              <div className='card-footer'>
                 <button
-                  className="btn btn-sm btn-success float-right"
-                  type="submit"
+                  className='btn btn-sm btn-success float-right'
+                  type='submit'
                 >
-                  <i className="mdi mdi-gamepad-circle"></i> Añadir
+                  <i className='mdi mdi-gamepad-circle' /> Añadir
                 </button>
               </div>
             </form>
