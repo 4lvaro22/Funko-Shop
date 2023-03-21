4d2f6f3be7a4aed388bb2f10f35017e8998bfb57
josefco-02 <josefco_02@uma.es>
2023-03-21 17:53:45 +0100

    estructura y ejemplos de filtros

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -18,7 +18,7 @@ export const Home = () => {
                     <div className="col-4">
                         <span className="input-group mb-2 d-flex justify-content-center">
                             <input id="buscador" type="text" className="form-control" aria-label="Buscador" aria-describedby="Buscar" />
-                            <button className="btn btn-outline-dark bg-secondary" type="button" id="searchButton"><i className="bi bi-search"></i></button>
+                            <button className="btn btn-outline-dark bg-primary" type="button" id="searchButton"><i className="bi bi-search"></i></button>
                         </span>
                     </div>
                     <div className="col-4"></div>
@@ -27,13 +27,56 @@ export const Home = () => {
 
             <div className="row align mx-auto">
                 {/* <-- Lado --> */}
-                <aside className="col-3 bg-info m-4">  {/* <-- Para el menú de selección de marca (a modo de filtro): Marvel, GoT, Harry Potter... --> */}
-                    <h2 className="text-center">Filtros</h2>
+                <aside className="col-2 m-4">  {/* <-- Para el menú de selección de marca (a modo de filtro): Marvel, GoT, Harry Potter... --> */}
+                    <h2 className="text-center border-bottom border-top">Filtros</h2>
+                    <div className="border rounded p-2">
+                        <div class="list-group">
+                            <h6 className="mx-2">Marca:</h6>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                Marvel
+                            </label>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                Harry Potter
+                            </label>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                Juego de Tronos
+                            </label>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                Star Wars
+                            </label>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                Dragon Ball
+                            </label>
+                        </div>
+
+                        <div class="list-group">
+                            <h6 className="mx-2">Precio:</h6>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                Menor a 10€
+                            </label>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                10€ - 20€
+                            </label>
+                            <label class="list-group-item border-0">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                20€ - 30€
+                            </label>
+                        </div>
+
+                    </div>
+
                 </aside>
 
                 {/* <-- Seccion --> */}
-                <section className="col-8 bg-primary m-4">
-                    <h2 className="text-center">Catálogo</h2>
+                <section className="col-9 m-4">
+                    <h2 className="text-center border-bottom border-top">Catálogo</h2>
                     <funko />
                 </section>
             </div>
