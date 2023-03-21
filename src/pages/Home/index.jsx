757505d0d4f1037630d56911bd104efce37f16f0
4lvaro_22 <pl.alvaro43@uma.es>
2023-03-21 17:47:12 +0100

    Search icon animation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,37 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { funko } from './../../components/funko';
 
 export const Home = () => {
-  console.log('Home');
+  const [form, toggleForm] = useState(false);
+  const [search, setSearch] = useState('');
 
-    console.log("Home");
+  useEffect(() => {
+    if (search.length > 0) {
+      toggleForm(true);
+    } else {
+      toggleForm(false);
+    }
+  }, [search]);
 
-    return (
-        <>
-        <nav className="container-fluid bg-dark">
-            <div className="row">
-                <div className="col-4"></div>
-                <div className="col-4">
-                    <span className="input-group mb-2 d-flex justify-content-center">
-                        <input type="text" className="form-control" aria-label="Buscador" aria-describedby="Buscar"/>
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><i className="bi bi-search"></i></button>
-                    </span>
-                </div>
-                <div className="col-4"></div>
-            </div>
-        </nav>
+  return (
+    <>
+      <nav className='container-fluid bg-dark'>
+        <div className='row'>
+          <div className='col-4' />
+          <div className='col-4'>
+            <span className='input-group mb-2 d-flex justify-content-center'>
+              <input
+                type='text' className='form-control' id='buscador' aria-label='Buscador' aria-describedby='Buscar'
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+              />
+              <button className={'btn btn-outline-' + (form ? 'primary' : 'secondary')} type='button' id='button-addon2'><i className='bi bi-search' /></button>
+            </span>
+          </div>
+          <div className='col-4' />
+        </div>
+      </nav>
 
       {/* <-- Lado --> */}
       <aside>  {/* <-- Para el menú de selección de marca (a modo de filtro): Marvel, GoT, Harry Potter... --> */}
