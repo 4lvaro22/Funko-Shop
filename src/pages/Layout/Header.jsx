a6d7ea2497761e4e148acf93dae4b41b7b9252d2
josefco-02 <josefco_02@uma.es>
2023-03-22 18:11:20 +0100

    arreglado header

--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -6,17 +6,21 @@ export const Header = () => {
     <>
       <header>
         <div className='container-fluid'>
-          <div className='center-block row bg-dark'>
-            <span className='col-4 d-flex align-self-center'>
+          <div className='center-block row bg-dark p-1'>
+            <span className='col-4 d-flex my-auto'>
               <Link to='/'><button class='btn btn-light my-2'><i class='bi bi-house-door-fill' /></button></Link>
             </span>
 
-            <span className='col-4 text-white row d-flex justify-content-center my-1'>
-              <img src={icono} className='col-2 img-fluid mx-2 mt-1' />
-              <h1 className='col-5 mt-1'>FunkoShop</h1>
+            <span className='col-4 text-white d-flex justify-content-center my-auto'>
+              <Link to='/' className='align-self-center'>
+                <img src={icono} className='mx-1 ' height={44} width={44} />
+              </Link>
+              <Link to='/' className='text-decoration-none align-self-center'>
+                <h1 className='text-white'>FunkoShop</h1>
+              </Link>
             </span>
 
-            <span className='col-4 d-flex align-self-center justify-content-end'>
+            <span className='col-4 d-flex align-self-center justify-content-end my-auto'>
               <Link to='/Login'><button id='login' class=' btn btn-light mx-1'>Iniciar Sesión</button></Link>
               <Link to='/SignUp'><button id='signup' class='btn btn-light mx-1'>Registrarse</button></Link>
               <Link to='/Cart'><button id='cart' class='btn btn-light mx-3'><i class='bi bi-cart' />Carrito</button></Link>
