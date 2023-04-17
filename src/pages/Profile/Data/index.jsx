9b888bb260acce3ef6346f69e8484ff069bd0c8f
Olivier-GG <100834647+Olivier-GG@users.noreply.github.com>
2023-04-17 17:33:23 +0200

    pop up cuando se guardan los datos

--- src/pages/Profile/Data/index.jsx
+++ src/pages/Profile/Data/index.jsx
@@ -33,10 +33,29 @@ const Data = ({ usuario, updateUsuario }) => {
             <input
               value='Guardar' name='save' type='button'
               className='btn btn-success col-lg-2 col-md-3 col-sm-4 '
+              data-bs-toggle='modal' data-bs-target='#exampleModal'
               onClick={() => {
                 updateUsuario(usuarioEditado);
               }}
             />
+
+            <div className='modal fade' id='exampleModal' tabIndex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+              <div className='modal-dialog'>
+                <div className='modal-content'>
+                  <div className='modal-header'>
+                    <h5 className='modal-title' id='exampleModalLabel'>Datos Guardados</h5>
+                    <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close' />
+                  </div>
+                  <div className='modal-body'>
+                    Todos los datos han sido actualizados correctamente.
+                  </div>
+                  <div className='modal-footer'>
+                    <button type='button' className='btn btn-primary' data-bs-dismiss='modal'>Cerrar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div className='col-lg-auto col-md-4 col-sm-4' />
           </div>
         </div>
