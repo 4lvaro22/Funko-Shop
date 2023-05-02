6feaca3432c543ed0ebd2fd52c990eb7ef95c00a
josefco-02 <josefco_02@uma.es>
2023-05-02 17:31:43 +0200

    revisado perfil

--- src/pages/Profile/Data/Input.jsx
+++ src/pages/Profile/Data/Input.jsx
@@ -3,7 +3,7 @@ const Input = ({ label, name, type, value, onChange, toFocus }) => {
   return (
     <div className='row mx-auto align-items-center p-2'>
       <div className='col-md-1' />
-      <h5 for={name} className='col-sm-5 col-md-4 col-lg-3 form-label'>{label}:</h5>
+      <h4 for={name} className='col-sm-5 col-md-4 col-lg-3 form-label'>{label}:</h4>
       <div className='col-sm-6 col-md-5 col-lg-auto me-auto float-start'>
         <input ref={toFocus} id={name} className='form-control' type={type} value={value} onChange={onChange} aria-label={label} />
       </div>
