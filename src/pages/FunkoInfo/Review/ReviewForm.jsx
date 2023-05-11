58d840616864b2802e6fe75663ca31242a727f3a
iLoopBreak <teogamer908@gmail.com>
2023-05-11 18:35:55 +0200

    refactor: Added ReviewForm to modals and fixed accesibility

--- src/pages/FunkoInfo/Review/ReviewForm.jsx
+++ src/pages/FunkoInfo/Review/ReviewForm.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Stars } from '../Stars';
+import { useModal } from '../../../components/Modal';
 
 export const ReviewForm = ({ sendReview }) => {
-  const [title, setValueTitle] = useState('');
-  const [review, setValueReview] = useState('');
+  const titleRef = useRef();
+  const reviewRef = useRef();
   const [rating, setRating] = useState(0);
-
-  const validate = () => {
-    return title.length && review.length;
-  };
+  const [reviewModal, openReviewModal] = useModal({ type: 'review' });
 
   const updateRating = (rating) => {
     setRating(rating);
@@ -16,15 +14,23 @@ export const ReviewForm = ({ sendReview }) => {
 
   return (
     <fieldset className='border rounded py-3'>
+      {reviewModal}
       <legend className='w-auto mx-5 px-5'><h2>-Escribe tu opinión-</h2></legend>
-      <form className=''>
+      <form onSubmit={(event) => {
+        if (titleRef.current.checkValidity() && reviewRef.current.checkValidity()) {
+          event.preventDefault();
+          sendReview(titleRef.current.value, reviewRef.current.value, rating);
+          openReviewModal();
+        }
+      }}
+      >
         <div className='row my-5'>
           <div className='row my-3'>
             <span className='text-end col-2'>
               <h4><label htmlFor='titulo'>Título:</label></h4>
             </span>
             <span className='col-md-9 mx-2'>
-              <input type='text' className='form-control w-25 col-1' id='titulo' onChange={(e) => setValueTitle(e.target.value)} required />
+              <input ref={titleRef} type='text' className='form-control w-25 col-1' id='titulo' required />
             </span>
           </div>
           <div className='row my-3'>
@@ -32,7 +38,7 @@ export const ReviewForm = ({ sendReview }) => {
               <h4><label htmlFor='opinion'>Escribe tu opinión:</label></h4>
             </span>
             <span className='col-md-9 mx-2'>
-              <textarea type='text' className='form-control w-25 col-1' id='opinion' onChange={(e) => setValueReview(e.target.value)} required />
+              <textarea ref={reviewRef} type='text' className='form-control w-25 col-1' id='opinion' required />
             </span>
           </div>
           <div className='row my-3'>
@@ -45,25 +51,7 @@ export const ReviewForm = ({ sendReview }) => {
           </div>
           <div className='row'>
             <span className='col-4 d-flex justify-content-center'>
-              <button disabled={!validate()} style={{ color: (!validate()) ? '#000000' : '#ffffff' }} onClick={() => { sendReview(title, review, rating); }} className='btn btn-primary' data-bs-toggle='modal' data-bs-target='#botonReview'>Publicar</button>
-
-              <div className='modal fade' id='botonReview' tabIndex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                <div className='modal-dialog'>
-                  <div className='modal-content'>
-                    <div className='modal-header'>
-                      <h5 className='modal-title' id='exampleModalLabel'><i className='bi bi-check' /> Opinión publicada</h5>
-                      <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close' />
-                    </div>
-                    <div className='modal-body'>
-                      Su opinión ha sido publicada correctamente.
-                    </div>
-                    <div className='modal-footer'>
-                      <button type='button' className='btn btn-primary' data-bs-dismiss='modal'>Cerrar</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
+              <button type='submit' className='btn btn-primary'>Publicar</button>
             </span>
           </div>
         </div>
